32601919262b7c39249abf04fcb07ee453995722
GustavoGJesus <ggomesdejesus01@gmail.com>
2022-12-03 17:47:44 -0300

    refactor: 💡 add new font

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -19,12 +19,10 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=swap"
-            rel="stylesheet"
-          />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Kanit:wght@400;500;600&display=swap" rel="stylesheet" />
+
         </Head>
         <body>
           <Main />
